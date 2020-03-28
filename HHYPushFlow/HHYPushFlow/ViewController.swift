08e784b18ce8d09b00195c6e602f78435b52c2dc
华惠友 <793316968@qq.com>
2020-03-28 17:36:56 +0800

    no message

--- HHYPushFlow/HHYPushFlow/ViewController.swift
+++ HHYPushFlow/HHYPushFlow/ViewController.swift
@@ -27,8 +27,11 @@ class ViewController: UIViewController {
         
     }
     @IBAction func startPushFlow(_ sender: Any) {
+        // 开始推流
         let stream = LFLiveStreamInfo()
-        stream.url = "rtmp://192.168.2.148:8080/live/demo"
+//        stream.url = "rtmp://192.168.2.148:8080/live/demo"
+        stream.url = "rtmp://202.69.69.180:443/live/aaa"
+        // 播放地址:rtmp://202.69.69.180:443/live/aaa
         session.startLive(stream)
         
         session.running = true
@@ -41,7 +44,7 @@ extension ViewController: LFLiveSessionDelegate {
     }
     
     func liveSession(_ session: LFLiveSession?, errorCode: LFLiveSocketErrorCode) {
-        
+        print("errorCode:\(errorCode)")
     }
     
     func liveSession(_ session: LFLiveSession?, liveStateDidChange state: LFLiveState) {
